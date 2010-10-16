dc56735e888a3747e1c4981bc0321b1b223965e3
Hay <huskyr@gmail.com>
2010-10-16 18:55:37 +0200

    i guess this works...

--- canvas.js
+++ canvas.js
@@ -1,77 +1,68 @@
-var canvas = $('#canvas'),
-    ctx = canvas.getContext('2d'),
-    imgd = ctx.createImageData(canvas.width, canvas.height),
-    px = imgd.data,
-    timer,
-    xd = 2.5, yd = 2, xc = -0.5, yc = -0.5, maxiter = 100, globaliter = 0,
-    startTime = +new Date();
-    
-$("#maxiter").value = maxiter;
-$("output[for*=maxiter]").innerHTML = maxiter;
-$("#maxiter").onchange = function() {
-    maxiter = this.value;
-    $("output[for*=maxiter]").innerHTML = this.value;
-}
-    
-function $(query) {
-    var el = document.querySelectorAll(query);
-    return (el.length == 1) ? el[0] : el;
-}
+window.App = {
+    "config" : {},
+    "main" : function() {
+        var canvas = $('#canvas'),
+            ctx = canvas.getContext('2d'),
+            imgd = ctx.createImageData(canvas.width, canvas.height),
+            px = imgd.data,
+            timer,
+            xd = 2.5, yd = 2, xc = -0.5, yc = -0.5, maxiter = 100, globaliter = 0,
+            startTime = +new Date();
 
-function calc(x, y) {
-    x = xd * (x / canvas.width + xc);
-    y = yd * (y / canvas.height + yc);
+        this.config = {
+            canvasWidth : canvas.width,
+            canvasHeight : canvas.height,
+            xd : xd,
+            yd : yd,
+            xc : xc,
+            yc : yc,
+            maxiter : maxiter
+        };
 
-    var x0 = x,
-        y0 = y,
-        iteration = 0;
+        $("#maxiter").value = maxiter;
+        $("output[for*=maxiter]").innerHTML = maxiter;
+        $("#maxiter").onchange = function() {
+            maxiter = this.value;
+            $("output[for*=maxiter]").innerHTML = this.value;
+        }
 
-    while (x * x + y * y <= 4 && iteration < maxiter) {
-        var xtemp = x*x - y*y + x0;
-        y = 2*x*y + y0;
-        x = xtemp;
-        iteration++;
-    }
+        function $(query) {
+            var el = document.querySelectorAll(query);
+            return (el.length == 1) ? el[0] : el;
+        }
 
-    return Math.round(255 * iteration / maxiter);
-}
+        function animate() {
+            var time = (+new Date()) - startTime;
+            $("h1 i").innerHTML = Number(1000 / time).toFixed(2) + " fps";
+            startTime = +new Date();
 
-function setPixel(x,y,c) {
-    var i = (x + y * canvas.width) * 4;
-    px[i + 0] = c[0];
-    px[i + 1] = c[1];
-    px[i + 2] = c[2];
-    px[i + 3] = c[3] || 255;
-}
+            // Start a new worker
+            var worker = new Worker('worker.js');
+            worker.postMessage(App.config);
+            worker.onmessage = function(event) {
+                console.log('data', event.data);
+                // px = event.data;
+            }
 
-function animate() {
-    var time = (+new Date()) - startTime;
-    $("h1 i").innerHTML = Number(1000 / time).toFixed(2) + " fps";
-    startTime = +new Date();
-    
-    for (x = 0; x < canvas.width; x++) {
-        for (y = 0; y < canvas.height; y++) {
-            var c = calc(x,y);
-            setPixel(x,y, [255, 0, 0, c]);
+            globaliter++;
+            $("h1 b").innerHTML = globaliter;
+
+            timer = setTimeout(function() {
+                ctx.putImageData(imgd, 0, 0);
+                xd -= 0.1;
+                yd -= 0.1;
+                xc -= 0.05;
+                yc -= 0.05;
+                // animate();
+            }, 40);
+        }
+
+        $("#start").onclick = function() {
+            animate();
+        }
+
+        $("#stop").onclick = function() {
+            clearTimeout(timer);
         }
     }
-    globaliter++;
-    $("h1 b").innerHTML = globaliter;
-
-    timer = setTimeout(function() {
-        ctx.putImageData(imgd, 0, 0);
-        xd -= 0.1;
-        yd -= 0.1;
-        xc -= 0.05;
-        yc -= 0.05;
-        animate();
-    }, 40);
-}
-
-$("#start").onclick = function() {
-    animate();
-}
-
-$("#stop").onclick = function() {
-    clearTimeout(timer);
-}
+};

6b64d70b74ded09404b2fc6b0e305a128835b06c
Hay <huskyr@gmail.com>
2010-10-16 18:11:03 +0200

    added some performance indicators

--- canvas.js
+++ canvas.js
@@ -1,26 +1,32 @@
-var canvas = document.getElementById('canvas'),
+var canvas = $('#canvas'),
     ctx = canvas.getContext('2d'),
     imgd = ctx.createImageData(canvas.width, canvas.height),
     px = imgd.data,
     timer,
-    xd = 2.5, yd = 2, xc = -0.5, yc = -0.5, max_iteration = 100;
+    xd = 2.5, yd = 2, xc = -0.5, yc = -0.5, maxiter = 100, globaliter = 0,
+    startTime = +new Date();
+    
+function $(query) {
+    var el = document.querySelectorAll(query);
+    return (el.length == 1) ? el[0] : el;
+}
 
-function computeColor(x, y) {
-    x = xd * (x /canvas.width + xc);
-    y = yd * (y /canvas.height + yc);
+function calc(x, y) {
+    x = xd * (x / canvas.width + xc);
+    y = yd * (y / canvas.height + yc);
 
     var x0 = x,
         y0 = y,
         iteration = 0;
 
-    while (x * x + y * y <= 4 && iteration < max_iteration ) {
+    while (x * x + y * y <= 4 && iteration < maxiter) {
         var xtemp = x*x - y*y + x0;
         y = 2*x*y + y0;
         x = xtemp;
         iteration++;
     }
 
-    return Math.round(255 * iteration / max_iteration);
+    return Math.round(255 * iteration / maxiter);
 }
 
 function setPixel(x,y,c) {
@@ -32,12 +38,18 @@ function setPixel(x,y,c) {
 }
 
 function animate() {
+    var time = (+new Date()) - startTime;
+    $("h1 i").innerHTML = Math.round(1000 / time, 3) + "fps";
+    startTime = +new Date();
+    
     for (x = 0; x < canvas.width; x++) {
         for (y = 0; y < canvas.height; y++) {
-            var c = computeColor(x,y);
+            var c = calc(x,y);
             setPixel(x,y, [255, 0, 0, c]);
         }
     }
+    globaliter++;
+    $("h1 b").innerHTML = globaliter;
 
     timer = setTimeout(function() {
         ctx.putImageData(imgd, 0, 0);
@@ -46,6 +58,11 @@ function animate() {
         // xc += 0.1;
         animate();
     }, 40);
+}
+
+$("#stop").onclick = function() {
+    alert('klik');
+    clearTimeout(timer);
 }
 
 animate();
